8e7a99473a6ab62d0955af43da4fa0c575101b5a
ducnt05 <nahduc05@gmail.com>
2026-03-15 00:04:22 +0700

    update code

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ window.addEventListener("scroll", function () {
 
     if (position < screenHeight - 100) {
       projects[i].classList.add("active");
-    }
+    } else {
+    projects[i].classList.remove("active");
+  }
   }
 });
 const icon = document.querySelectorAll(".icon-skill img");
@@ -19,6 +21,8 @@ window.addEventListener("scroll", function () {
 
     if (position < screenHeight - 100) {
       icon[i].classList.add("active");
+    } else {
+      icon[i].classList.remove("active");
     }
   }
 });
@@ -31,6 +35,8 @@ window.addEventListener("scroll", function () {
 
     if (position < screenHeight - 100) {
       imgProject[i].classList.add("active");
+    } else {
+      imgProject[i].classList.remove("active");
     }
   }
 });
@@ -63,15 +69,49 @@ window.addEventListener("scroll", function () {
 
   if (position < screenHeight - 100) {
     martWork.classList.add("active");
+  } else {
+    martWork.classList.remove("active")
   }
 });
-setInterval(function(){
-    let btn = document.querySelector(".header button");
+setInterval(function () {
+  let btn = document.querySelector(".header button");
 
-    btn.style.transform = "translateY(-5px)";
+  btn.style.transform = "translateY(-5px)";
 
-    setTimeout(function(){
-        btn.style.transform = "translateY(0px)";
-    },300);
+  setTimeout(function () {
+    btn.style.transform = "translateY(0px)";
+  }, 300);
+}, 1000);
 
-},1000);
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide() {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+
+  index++;
+  if (index > slides.length) {
+    index = 1;
+  }
+
+  slides[index - 1].style.display = "block";
+
+  setTimeout(showSlide, 10000);
+}
+
+showSlide();
+
+const myCv = document.querySelector(".my-cv");
+
+window.addEventListener("scroll", function () {
+  const positionTop = myCv.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+
+  if (positionTop < screenHeight - 100) {
+    myCv.classList.add("active");
+  } else {
+    myCv.classList.remove("active");
+  }
+});
